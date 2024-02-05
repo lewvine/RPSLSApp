ff8ef1fe6105c58febe0e3685b648adfd970a32d
Lewis Vine <lew.vine@gmail.com>
2024-02-05 12:25:30 -0500

    Added classes

---
+++ Gesture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSApp
+{
+    internal abstract class Gesture
+    {
+        string name;
+
+        public Gesture(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract bool CompareGestures();
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSApp
+{
+    internal abstract class Player
+    {
+        public Gesture selectedGesture;
+        public List<Gesture> gestures;
+        //Needs to implement a choose gestue method.
+        //Needs to have a list of possible gestures.
+        public Player()
+        {
+            this.selectedGesture = null;
+            this.gestures = new List<Gesture>() { ""}
+        }
+        public abstract Gesture ChooseGesture();
+    }
+}
